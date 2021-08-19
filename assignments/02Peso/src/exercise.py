a0d4c61fbf71c1a04dfbb706ada1580b6a1a4067
A00833404 <A00833404@itesm.mx>
2021-08-19 17:54:30 -0500

    hola

--- assignments/02Peso/src/exercise.py
+++ assignments/02Peso/src/exercise.py
@@ -1,6 +1,12 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    print("Programa para bajar de peso.")
+    ini = float(input("Introduzca su peso actual: "))
+    fin = float(input("Ahora, introduzca el peso al que desee llegar: "))
+    mes = int(input("Por último, introduzca el lapso en el que lo desee completar (meses): "))
+    cal = (ini - fin)/(mes)
+    print("Lo que debes bajar por mes es: " + str(cal))
+ 
 
 
 
