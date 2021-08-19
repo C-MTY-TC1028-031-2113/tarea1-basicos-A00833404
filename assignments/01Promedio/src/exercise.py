0be16b5f5c338bd47b2f3f292114521ddef70874
A00833404 <A00833404@itesm.mx>
2021-08-19 14:08:47 -0500

    hola

--- assignments/01Promedio/src/exercise.py
+++ assignments/01Promedio/src/exercise.py
@@ -1,7 +1,13 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
-
+    print("Programa para calcular promedio.")
+    mat1 = float(input("Introduzca su primera calificación: "))
+    mat2 = float(input("Ahora la segunda: "))
+    mat3 = float(input("Enseguida, la tercera: "))
+    mat4 = float(input("Por último, la cuarta: "))
+    prom = (mat1 + mat2 + mat3 + mat4)/4
+    print("El promedio se sus calificaciones es: " + str(prom))
+main()
 
 if __name__ == '__main__':
     main()
