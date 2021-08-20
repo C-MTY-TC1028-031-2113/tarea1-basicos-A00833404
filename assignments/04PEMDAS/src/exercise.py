bab17297d13edc5bd2ed0f441b4c1a1dfa740ebb
A00833404 <A00833404@itesm.mx>
2021-08-19 19:00:09 -0500

    ejercicio 5

--- assignments/04PEMDAS/src/exercise.py
+++ assignments/04PEMDAS/src/exercise.py
@@ -1,6 +1,12 @@
+import math
+
 def main():
     from math import sqrt
-    pass
+    oper1 = 2 &frac34; + 4 &frac23; - 3 &frac15; + 5 &frac12;</p>
+    oper2 = 2 <span>&#8730;</span>35<sup>2</sup> + 4 <span>&#8730;</span>36<sup>3</sup> - 6<span>&#8730;</span>49<sup>2</sup></p>
+    oper3 = (a<sup>3</sup> + 2b<sup>2</sup>) / (4a)</p>
+    oper4 = (2(a+b)<sup>2</sup> + 4(a-b)<sup>2</sup>) / ab<sup>2</sup></p>
+    oper5 = <span>&#8730;</span>((a+b)<sup>2</sup> + 2<sup>(a+b)</sup>) / (2a + 2b)<sup>2</sup></p>
     
    
 
--- assignments/05CuentaBancaria/src/exercise.py
+++ assignments/05CuentaBancaria/src/exercise.py
@@ -1,6 +1,14 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    ant = float(input("Ingrese el saldo del mes anterior: "))
+    ing = float(input("Ingresos: "))
+    egr = float(input("Egresos: "))
+    ja = int(input("Número de cheques expedidos: "))
+    che = ja * 13
+    fin = ant + ing - egr - che
+    res = (fin * 0.075) 
+    print("Saldo del final del mes: " + str(fin-res))
+
 
 
 
